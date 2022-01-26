6b4cc22569af92b5abae1ef859aff64402794280
adwitkow <adamadik@poczta.fm>
2022-01-26 16:19:16 +0100

    Add StringIdRule

---
+++ KenshiWikiValidator.Tests/Features/ArticleValidation/Validators/Rules/StringIdRuleTests.cs
@@ -0,0 +1,59 @@
+﻿using KenshiWikiValidator.Features.ArticleValidation.Validators.Rules;
+using KenshiWikiValidator.Features.DataItemConversion;
+using KenshiWikiValidator.Features.DataItemConversion.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.IO;
+
+namespace KenshiWikiValidator.Tests.Features.ArticleValidation.Validators.Rules
+{
+    [TestClass]
+    public class StringIdRuleTests
+    {
+        private string incorrectResourceContent;
+        private string correctResourceContent;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.incorrectResourceContent = File.ReadAllText(@"TestResources\WeaponArticleValidatorIncorrectResource.txt");
+            this.correctResourceContent = File.ReadAllText(@"TestResources\WeaponArticleValidatorCorrectResource.txt");
+        }
+
+        [TestMethod]
+        public void ShouldSucceedIfArticleContainsCorrectStringId()
+        {
+            var wakizashi = new Weapon()
+            {
+                Name = "Wakizashi",
+                StringId = "1020-gamedata.base"
+            };
+
+            var itemRepository = new Mock<IItemRepository>();
+            itemRepository.Setup(repo => repo.GetItems()).Returns(new[] { wakizashi });
+            var rule = new StringIdRule(itemRepository.Object);
+
+            var result = rule.Execute("Wakizashi", this.correctResourceContent);
+
+            Assert.IsTrue(result.Success);
+        }
+
+        [TestMethod]
+        public void ShouldFailIfArticleContainsIncorrectStringId()
+        {
+            var wakizashi = new Weapon()
+            {
+                Name = "Wakizashi",
+                StringId = "1020-gamedata.base"
+            };
+
+            var itemRepository = new Mock<IItemRepository>();
+            itemRepository.Setup(repo => repo.GetItems()).Returns(new[] { wakizashi });
+            var rule = new StringIdRule(itemRepository.Object);
+
+            var result = rule.Execute("Wakizashi", this.incorrectResourceContent);
+
+            Assert.IsFalse(result.Success);
+        }
+    }
+}
---
+++ KenshiWikiValidator/Features/ArticleValidation/Validators/Rules/StringIdRule.cs
@@ -0,0 +1,60 @@
+﻿using KenshiWikiValidator.Features.DataItemConversion;
+
+namespace KenshiWikiValidator.Features.ArticleValidation.Validators.Rules
+{
+    public class StringIdRule : IValidationRule
+    {
+        private readonly IItemRepository itemRepository;
+
+        public StringIdRule(IItemRepository itemRepository)
+        {
+            this.itemRepository = itemRepository;
+        }
+
+        public RuleResult Execute(string title, string content)
+        {
+            var result = new RuleResult();
+
+            using var reader = new StringReader(content);
+
+            string? line;
+            var matchingItems = itemRepository
+                .GetItems()
+                .Where(item => title.ToLower().Trim().Equals(item.Name.ToLower().Trim()))
+                .ToList();
+            var stringIdFound = false;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains("string id"))
+                {
+                    var pairs = line.Split("|");
+                    var elements = pairs.SelectMany(pair => pair.Split("=")).ToArray();
+                    for (var i = 0; i < elements.Length; i++)
+                    {
+                        var element = elements[i].Trim();
+                        if (element.Contains("string id"))
+                        {
+                            var stringId = elements[i + 1].Trim();
+
+                            var matchingItem = matchingItems.FirstOrDefault(item => item.StringId == stringId);
+
+                            if (matchingItem is null)
+                            {
+                                result.AddIssue($"String id '{stringId}' is incorrect in the article. Should be corrected to one of the following: [{string.Join(", ", matchingItems.Select(item => item.StringId))}]");
+                            }
+
+                            stringIdFound = true;
+                        }
+                    }
+                }
+            }
+
+            if (!stringIdFound)
+            {
+                result.AddIssue($"No string id! Most likely string id: [{string.Join(", ", matchingItems.Select(item => item.StringId))}]");
+            }
+
+            return result;
+        }
+    }
+}
