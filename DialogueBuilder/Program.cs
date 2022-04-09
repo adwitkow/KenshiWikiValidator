22cc6df36de16d4db7886f668abc4f2177209a23
adwitkow <adamadik@poczta.fm>
2022-04-09 19:42:15 +0200

    Properly replace speaker names

--- DialogueBuilder/Program.cs
+++ DialogueBuilder/Program.cs
@@ -21,7 +21,7 @@ var beep = characters.Single(character => character.Name == "Beep");
 
 WriteCharacterDialogue(repository, dialogueBuilder, beep);
 
-void WriteDialogueTree(WikiSectionBuilder sectionBuilder, int level, IEnumerable<DialogueLine> dialogueLines, Dictionary<DialogueSpeaker, string> speakers, Stack<DialogueLine> dialogueStack)
+void WriteDialogueTree(WikiSectionBuilder sectionBuilder, int level, IEnumerable<DialogueLine> previousLines, IEnumerable<DialogueLine> dialogueLines, Dictionary<DialogueSpeaker, IEnumerable<string>> speakersMap, Stack<DialogueLine> dialogueStack)
 {
     foreach (var line in dialogueLines)
     {
@@ -32,6 +32,14 @@ void WriteDialogueTree(WikiSectionBuilder sectionBuilder, int level, IEnumerable
 
         dialogueStack.Push(line);
 
+        var newSpeakersMap = speakersMap;
+        if (line.SpeakerIsCharacter.Any())
+        {
+            var characterNames = line.SpeakerIsCharacter.Select(character => character.Name);
+            newSpeakersMap = new Dictionary<DialogueSpeaker, IEnumerable<string>>(speakersMap);
+            newSpeakersMap[line.Speaker] = characterNames;
+        }
+
         var text = (string)line.Properties["text0"];
         var isInterjection = false;
         if (string.IsNullOrEmpty(text))
@@ -40,11 +48,22 @@ void WriteDialogueTree(WikiSectionBuilder sectionBuilder, int level, IEnumerable
         }
         else
         {
-            sectionBuilder.WithLine($"{new string('*', level)} '''{speakers[line.Speaker]}''': {text}");
+            var validSpeakers = newSpeakersMap[line.Speaker];
+            string speakers;
+            if (validSpeakers.Count() > 1)
+            {
+                speakers = string.Join(", ", validSpeakers.SkipLast(1)) + " or " + validSpeakers.TakeLast(1).Single();
+            }
+            else
+            {
+                speakers = validSpeakers.Single();
+            }
+
+            sectionBuilder.WithLine($"{new string('*', level)} '''{speakers}''': {text}");
         }
 
         int nextLevel;
-        if ((line.Lines.Count() > 1 || dialogueLines.Count() > 1) && !isInterjection)
+        if ((line.Lines.Count() > 1 || dialogueLines.Count() > 1 || previousLines.Count() > 1) && !isInterjection)
         {
             nextLevel = level + 1;
         }
@@ -53,7 +72,7 @@ void WriteDialogueTree(WikiSectionBuilder sectionBuilder, int level, IEnumerable
             nextLevel = level;
         }
 
-        WriteDialogueTree(sectionBuilder, nextLevel, line.Lines, speakers, dialogueStack);
+        WriteDialogueTree(sectionBuilder, nextLevel, dialogueLines, line.Lines, newSpeakersMap, dialogueStack);
 
         dialogueStack.Pop();
     }
@@ -66,38 +85,63 @@ void WriteCharacterDialogue(ItemRepository repository, DialogueBuilder dialogueB
 
     var dialogues = packages.SelectMany(package => dialogueBuilder.Build(package).Dialogues).ToList();
 
-    var externalDialogues = repository.GetItems().OfType<DialoguePackage>()
-        .SelectMany(package => package.Dialogues);
+    var externalPackages = repository.GetItems().OfType<DialoguePackage>();
 
-    foreach (var dialogue in externalDialogues)
+    var dialogueIdTocharacter = dialogues.ToDictionary(dialogue => dialogue.StringId, _ => new[] { character.Name }.ToList());
+
+    foreach (var package in externalPackages)
     {
-        if (dialogue.SpeakerIsCharacter.Any(speaker => speaker.StringId == character.StringId))
-        {
-            dialogues.Add(dialogue);
-            continue;
-        }
+        var referencingItems = repository
+            .GetReferencingDataItemsFor(package.StringId);
+        var packageOwnerName = referencingItems.FirstOrDefault(item => item.Type == ItemType.Character)?.Name;
 
-        var linesQueue = new Queue<DialogueLine>(dialogue.Lines);
-        var processedLines = new HashSet<string>();
-        while (linesQueue.TryDequeue(out var dequeued))
+        var externalDialogues = package.Dialogues;
+        foreach (var dialogue in externalDialogues)
         {
-            foreach (var line in dequeued.Lines)
+            if (!dialogueIdTocharacter.TryGetValue(dialogue.StringId, out var list))
             {
-                if (!processedLines.Contains(line.StringId) && !linesQueue.Contains(line))
-                {
-                    linesQueue.Enqueue(line);
-                }
+                list = new List<string>();
+                dialogueIdTocharacter.Add(dialogue.StringId, list);
             }
 
-            processedLines.Add(dequeued.StringId);
+            if (!string.IsNullOrEmpty(packageOwnerName) && !list.Contains(packageOwnerName))
+            {
+                list.Add(packageOwnerName);
+            }
 
-            if (dequeued.SpeakerIsCharacter.Any(speaker => speaker.StringId == character.StringId))
+            if (dialogue.SpeakerIsCharacter.Any())
+            {
+                dialogueIdTocharacter[dialogue.StringId].Add(dialogue.SpeakerIsCharacter.Single().Name);
+            }
+
+            if (dialogue.SpeakerIsCharacter.Any(speaker => speaker.StringId == character.StringId))
             {
                 dialogues.Add(dialogue);
+                continue;
+            }
+
+            var linesQueue = new Queue<DialogueLine>(dialogue.Lines);
+            var processedLines = new HashSet<string>();
+            while (linesQueue.TryDequeue(out var dequeued))
+            {
+                foreach (var line in dequeued.Lines)
+                {
+                    if (!processedLines.Contains(line.StringId) && !linesQueue.Contains(line))
+                    {
+                        linesQueue.Enqueue(line);
+                    }
+                }
+
+                processedLines.Add(dequeued.StringId);
+
+                if (dequeued.SpeakerIsCharacter.Any(speaker => speaker.StringId == character.StringId))
+                {
+                    dialogues.Add(dialogue);
+                }
             }
         }
     }
-
+    
     dialogues = dialogues.Distinct().ToList();
 
     foreach (var dialogue in dialogues)
@@ -105,25 +149,36 @@ void WriteCharacterDialogue(ItemRepository repository, DialogueBuilder dialogueB
         var sectionBuilder = new WikiSectionBuilder();
         sectionBuilder.WithHeader($"{dialogue.Name} ({string.Join(", ", dialogue.Events)})");
 
-        var speakers = new Dictionary<DialogueSpeaker, string>()
+        var validCharacters = dialogueIdTocharacter[dialogue.StringId];
+        IEnumerable<string> mainSpeakers;
+        if (validCharacters.Any())
         {
-            { DialogueSpeaker.Me, character.Name },
-            { DialogueSpeaker.Target, "Target" },
-            { DialogueSpeaker.TargetIfPlayer, "Player" },
-            { DialogueSpeaker.Interjector1, "Interjector #1" },
-            { DialogueSpeaker.Interjector2, "Interjector #2" },
-            { DialogueSpeaker.Interjector3, "Interjector #3" },
-            { DialogueSpeaker.TargetWithRace, "TODO: TargetWithRace" },
-            { DialogueSpeaker.WholeSquad, "Whole Squad" },
+            mainSpeakers = validCharacters;
+        }
+        else
+        {
+            mainSpeakers = new[] { "Character" };
+        }
+
+        var speakers = new Dictionary<DialogueSpeaker, IEnumerable<string>>()
+        {
+            { DialogueSpeaker.Me, mainSpeakers },
+            { DialogueSpeaker.Target, new[] { "Target" } },
+            { DialogueSpeaker.TargetIfPlayer, new[] { "Player" } },
+            { DialogueSpeaker.Interjector1, new[] { "Interjector #1" } },
+            { DialogueSpeaker.Interjector2, new[] { "Interjector #2" } },
+            { DialogueSpeaker.Interjector3, new[] { "Interjector #3" } },
+            { DialogueSpeaker.TargetWithRace, new[] { "TODO: TargetWithRace" } },
+            { DialogueSpeaker.WholeSquad, new[] { "Whole Squad" } },
         };
 
         if (dialogue.Events.Count() == 1 && dialogue.Events.First() == DialogueEvent.EV_PLAYER_TALK_TO_ME)
         {
-            speakers[DialogueSpeaker.Target] = "Player";
+            speakers[DialogueSpeaker.Target] = new[] { "Player" };
         }
 
         var dialogueStack = new Stack<DialogueLine>();
-        WriteDialogueTree(sectionBuilder, 1, dialogue.Lines, speakers, dialogueStack);
+        WriteDialogueTree(sectionBuilder, 1, Enumerable.Empty<DialogueLine>(), dialogue.Lines, speakers, dialogueStack);
 
         var text = sectionBuilder.Build() + Environment.NewLine;
 
