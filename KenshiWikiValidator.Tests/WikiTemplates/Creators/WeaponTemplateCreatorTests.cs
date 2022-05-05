e30d6779601773ebd01d2e0cabbf69a3089bb3f3
adwitkow <adamadik@poczta.fm>
2022-05-05 17:34:05 +0200

    Add basic unit tests for WeaponTemplateCreator

---
+++ KenshiWikiValidator.Tests/WikiTemplates/Creators/WeaponTemplateCreatorTests.cs
@@ -0,0 +1,41 @@
+﻿using KenshiWikiValidator.BaseComponents;
+using KenshiWikiValidator.OcsProxy;
+using KenshiWikiValidator.OcsProxy.Models;
+using KenshiWikiValidator.WikiTemplates.Creators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace KenshiWikiValidator.Tests.WikiTemplates.Creators
+{
+    [TestClass]
+    public class WeaponTemplateCreatorTests
+    {
+        [TestMethod]
+        public void ShouldReturnNullForEmptyArticleData()
+        {
+            var repository = new Mock<IItemRepository>();
+            var creator = new WeaponTemplateCreator(repository.Object, new ArticleData());
+
+            var template = creator.Generate();
+
+            Assert.IsNull(template);
+        }
+
+        [TestMethod]
+        public void ShouldNotReturnNullForValidStringId()
+        {
+            var repository = new Mock<IItemRepository>();
+            repository
+                .Setup(repo => repo.GetItemByStringId<Weapon>("stringid"))
+                .Returns(new Weapon("stringid", "weapon name"));
+            var articleData = new ArticleData();
+            articleData.StringIds.Add("stringid");
+
+            var creator = new WeaponTemplateCreator(repository.Object, articleData);
+
+            var template = creator.Generate();
+
+            Assert.IsNotNull(template);
+        }
+    }
+}
