aab81bb4fb4b40ba03dad63eb2c1e4125ae0b0cf
adwitkow <adamadik@poczta.fm>
2022-05-14 17:16:06 +0200

    Add unit tests

--- tests/KenshiWikiValidator.Tests/WikiCategories/SharedRules/StringIdRuleTests.cs
+++ tests/KenshiWikiValidator.Tests/WikiCategories/SharedRules/StringIdRuleTests.cs
@@ -4,7 +4,7 @@ using KenshiWikiValidator.OcsProxy.Models;
 using KenshiWikiValidator.WikiCategories.SharedRules;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace KenshiWikiValidator.Tests.WikiCategories.SharedRules
 {
@@ -91,6 +91,48 @@ namespace KenshiWikiValidator.Tests.WikiCategories.SharedRules
             validator.Object.Validate("Lost Armoury", textToValidate);
             
             Assert.AreEqual("49935-rebirth.mod", validator.Object.Data.PotentialStringId);
+        }
+
+        [TestMethod]
+        public void ShouldFailWithCorrectStringIdIfFcsNameIsPresent()
+        {
+            var wakizashi = new Weapon("1020-gamedata.base", "Wakizashi");
+            var textToValidate = "{{Weapon|fcs_name = Wakizashi}}";
+
+            var itemRepository = new Mock<IItemRepository>();
+            itemRepository
+                .Setup(repo => repo.GetItems())
+                .Returns(new[] { wakizashi });
+
+            var validator = new Mock<ArticleValidatorBase>();
+            validator
+                .Setup(v => v.Rules)
+                .Returns(new[] { new StringIdRule(itemRepository.Object, new WikiTitleCache(), true) });
+
+            var result = validator.Object.Validate("Wakizashi - title different from the FCS name", textToValidate);
+
+            Assert.AreEqual("No string id! Most likely string id: [string id = 1020-gamedata.base|]", result.Issues.First());
+        }
+
+        [TestMethod]
+        public void ShouldFilterByTypeCorrectly()
+        {
+            var wakizashi = new Weapon("1020-gamedata.base", "Wakizashi");
+            var textToValidate = "{{Weapon|string id = 1020-gamedata.base}}";
+
+            var itemRepository = new Mock<IItemRepository>();
+            itemRepository
+                .Setup(repo => repo.GetItems<Weapon>())
+                .Returns(new[] { wakizashi });
+
+            var validator = new Mock<ArticleValidatorBase>();
+            validator
+                .Setup(v => v.Rules)
+                .Returns(new[] { new StringIdRule<Weapon>(itemRepository.Object, new WikiTitleCache(), true) });
+
+            var result = validator.Object.Validate("Wakizashi", textToValidate);
+
+            Assert.IsTrue(result.Success);
         }
     }
 }
