4426af36863d5b5e6854df51900fe2696e20766d
adwitkow <adamadik@poczta.fm>
2024-10-17 13:08:38 +0200

    Generate character table after CharactersArticleValidator finishes

--- src/KenshiWikiValidator/Characters/CharactersArticleValidator.cs
+++ src/KenshiWikiValidator/Characters/CharactersArticleValidator.cs
@@ -18,6 +18,7 @@ using KenshiWikiValidator.BaseComponents;
 using KenshiWikiValidator.BaseComponents.SharedRules;
 using KenshiWikiValidator.Characters.Rules;
 using KenshiWikiValidator.OcsProxy;
+using KenshiWikiValidator.OcsProxy.Models;
 using KenshiWikiValidator.WikiCategories.Characters.Rules;
 
 namespace KenshiWikiValidator.Characters
@@ -25,6 +26,8 @@ namespace KenshiWikiValidator.Characters
     public class CharactersArticleValidator : ArticleValidatorBase
     {
         private readonly IEnumerable<IValidationRule> rules;
+        private readonly IItemRepository itemRepository;
+        private readonly WikiTitleCache wikiTitles;
 
         public CharactersArticleValidator(IItemRepository itemRepository, WikiTitleCache wikiTitles)
             : base(itemRepository, wikiTitles)
@@ -36,6 +39,8 @@ namespace KenshiWikiValidator.Characters
                 new CharacterStatsRule(itemRepository),
                 new EquipmentSectionRule(itemRepository, wikiTitles),
             };
+            this.itemRepository = itemRepository;
+            this.wikiTitles = wikiTitles;
         }
 
         public override string CategoryName => "Characters";
@@ -57,6 +62,58 @@ namespace KenshiWikiValidator.Characters
             }
 
             return true;
+        }
+
+        public override IEnumerable<RuleResult> AfterValidations()
+        {
+            this.GenerateCharacterTable();
+
+            return Enumerable.Empty<RuleResult>();
+        }
+
+        private void GenerateCharacterTable()
+        {
+            var lines = new List<string>
+            {
+                "Automatically generated.",
+                string.Empty,
+                "{| class=\"article-table sortable\"",
+                "! String ID",
+                "! Article title (or FCS name)",
+                "! Missing or implicit string ID",
+            };
+
+            foreach (var character in this.itemRepository.GetItems<Character>().OrderBy(ch => ch.Name.Trim()))
+            {
+                var isUniqueReplacement = this.itemRepository.GetItems<Character>().Any(r => r.UniqueReplacementSpawn.ContainsItem(character));
+                var isSpecialLeader = this.itemRepository.GetItems<FactionCampaign>().Any(r => r.SpecialLeader.ContainsItem(character));
+                var isNewGameCharacter = this.itemRepository.GetItems<NewGameStartoff>().Any(r => r.Characters.ContainsItem(character));
+                var isInSquad = this.itemRepository.GetItems<Squad>().Any(r => r.ContainsCharacter(character));
+                var isInUniqueSquad = this.itemRepository.GetItems<UniqueSquadTemplate>().Any(r => r.Leader.ContainsItem(character) || r.Squad.ContainsItem(character));
+
+                if (isUniqueReplacement || isSpecialLeader || isNewGameCharacter
+                    || isInSquad || isInUniqueSquad)
+                {
+                    lines.Add("|-");
+                    lines.Add($"| {character.StringId.Trim()}");
+                    var name = this.wikiTitles.GetTitle(character);
+                    lines.Add($"| [[{name.Trim()}]]");
+
+                    if (this.wikiTitles.HasArticle(character))
+                    {
+                        lines.Add("| No");
+                    }
+                    else
+                    {
+                        lines.Add("| Yes");
+                    }
+                }
+            }
+
+            lines.Add("|}");
+
+            var path = Path.Combine("output", "character-table.txt");
+            File.WriteAllLines(path, lines);
         }
     }
 }
